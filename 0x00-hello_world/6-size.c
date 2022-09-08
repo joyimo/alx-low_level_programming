e8bd391d686d5ff9ce84e9d937d580a5b3f78270
Joy Imo <joyimo98@gmail.com>
2022-09-08 16:26:04 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,20 +3,20 @@
 /**
  * main - Entry point
  * 
- * Return: Always 0 (success)
+ * Return: Always 0 success
  */
 int main(void)
 {
-	char c;
 	int i;
+	char c;
 	long li
 	long long lli;
 	float f;
 
-	printf("Size of a char: %d byte(s)\n", sizeof(c));
-	printf("Size of an int: %d byte(s)\n", sizeof(i));
-	printf("Size of a long: %d byte(s)\n", sizeof(li));
-	printf("Size of a long long: %dbyte(s)\n", sizeof(lli));
+	printf("Size of a int: %d byte(s)\n", sizeof(i));
+	printf("Size of an char: %d byte(s)\n", sizeof(c));
+	printf("Size of a long int: %d byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %dbyte(s)\n", sizeof(lli));
 	printf("Size of a float: %d byte(s)\n", sizeof(f));
 	return (0);
 }
