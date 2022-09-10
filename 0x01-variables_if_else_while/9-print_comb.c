e300315047d2ff0112e968ef359716705eaa4d3c
Joy Imo <joyimo98@gmail.com>
2022-09-10 03:20:49 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 /**
  * main - printing numbers from 0-9 with commas and space between them
- *
  * Description: using the main function
  * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9"
  * Return: 0
@@ -13,10 +12,8 @@ int main(void)
 	for (c = 48; c <= 57; c++)
 	{
 		putchar(c);
-		if (c != 57									{
-													putchar(',');									putchar(' ');
-												}									}
-
+		if (c != 57									{											putchar(',');									putchar(' ');								}
+	}
 	putchar('\n');
 	return (0);
 }
