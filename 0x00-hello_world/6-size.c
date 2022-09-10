6ab0bb0c8714b1debea364adf32f0de627966f90
Joy Imo <joyimo98@gmail.com>
2022-09-10 01:29:31 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,6 +7,12 @@
  */
 int main(void)
 {
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
