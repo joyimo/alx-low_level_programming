6599148e0fb8ecb71666c3df4ad216dadc7f6de9
Joy Imo <joyimo98@gmail.com>
2022-09-11 11:44:59 -0700

    Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -14,7 +14,8 @@ int main(void)
 	{
 		putchar(c);
 		
-		if (c != 57)									{											putchar(',');									putchar(' ');								}
+		if (c != 57)									
+		{											putchar(',');									putchar(' ');								}
 	}
 	putchar('\n');
 	return (0);
