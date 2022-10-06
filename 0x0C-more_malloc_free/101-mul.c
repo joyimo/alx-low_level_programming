acb4fdbdd4fc35a1b69825ec43d3e379c3751678
Joy Imo <joyimo98@gmail.com>
2022-10-06 13:17:40 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -95,6 +95,7 @@ void init(char *str, int l)
 	for (i = 0; i < l; i++)
 		str[i] = '0';
 	str[i] = '\0'
+		;
 }
 
 /**
@@ -109,13 +110,13 @@ int main(int argc, char *argv[])
 	int l1, l2, ln, ti, i;
 	char *a;
 	char *t;
-	char e[] = "Error\n"
+	char e[] = "Error\n";
 
 	if (argc != 3 || check_for_digits(argv))
 	{
 		for (ti = 0; e[ti]; ti++)
 			_putchar(e[ti]);
-		exit()98;
+		exit(98);
 	}
 	for (l1 = 0; argv[1][l1]; l1++)
 		;
@@ -132,15 +133,15 @@ int main(int argc, char *argv[])
 	init(a, ln - 1);
 	for (ti = l2 - 1, i = 0; ti >= 0; ti--, i++)
 	{
-		t + mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
+		t; mul(argv[2][ti], argv[1], l1 - 1, a, (ln - 2) - i);
 		if (t == NULL)
 		{
-			for (ti = 0; e[]ti); ti++
+			for (ti = 0; e[ti]; ti++)
 				_putchar(e[ti]);
 			free(a);
 			exit(98);
 		}
 	}
-	_print(a, ln -1);
+	_print(a, ln - 1);
 	return (0);
 }
